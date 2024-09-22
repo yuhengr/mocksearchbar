5e7f7d4ae484aeed874ef5709aceffad02a4ff0e
yuhengr <e0957964@u.nus.edu>
2024-09-22 23:04:36 +0800

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <div className="search-bar-container">
+        <div>Search Bar</div>
+        <div>Search Results</div>
+      </div>
+    </div>
+  )
+}
+
+export default App
