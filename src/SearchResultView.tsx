37a5b14cd8334a6a317e3c5f0762ddd1bed2125b
yuhengr <e0957964@u.nus.edu>
2024-09-23 15:59:07 +0800

    add SearchResultView

---
+++ src/SearchResultView.tsx
@@ -0,0 +1,9 @@
+
+function SearchResultView() {
+
+    return(
+        <div className="search-result-view">This is a single search result.</div>
+    );
+}
+
+export default SearchResultView;
--- src/SearchResults.tsx
+++ src/SearchResults.tsx
@@ -1,3 +1,4 @@
+import SearchResultView from './SearchResultView.tsx';
 
 interface SearchResultsProps {
     results: string;
@@ -8,6 +9,7 @@ function SearchResults({results}:SearchResultsProps) {
     return(
         <div className="search-results-container">
             <p>The search was: {results}</p>
+            <SearchResultView />
         </div>
     );
 }
